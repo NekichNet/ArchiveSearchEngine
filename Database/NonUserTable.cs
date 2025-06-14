b35e41a4718b1bd3608cb727e64365ba2267b6c4
NekichNet <102032304+NekichNet@users.noreply.github.com>
2025-06-14 21:45:35 +0500

    Added exists and getunit methods to NonUserTable

--- Database/NonUserTable.cs
+++ Database/NonUserTable.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,15 +23,15 @@ namespace ArchiveSearchEngine.Database
                 "struct_division TEXT NOT NULL)", _connection).ExecuteNonQuery();
         }
 
-        public void NewUnit(NonUser unit)
+        public void NewUnit(string fullname, string post, string struct_division)
         {
-            unit.Fullname = unit.Fullname.Replace("'", "");
-            unit.Post = unit.Post.Replace("'", "");
-            unit.StructDivision = unit.StructDivision.Replace("'", "");
+            fullname = fullname.Replace("'", "");
+            post = post.Replace("'", "");
+            struct_division = struct_division.Replace("'", "");
 
             new SqliteCommand($"INSERT INTO NonUserTable " +
                 $"(fullname, post, struct_division) VALUES " +
-                $"('{unit.Fullname}', '{unit.Post}', '{unit.StructDivision}')", _connection).ExecuteNonQuery();
+                $"('{fullname}', '{post}', '{struct_division}')", _connection).ExecuteNonQuery();
         }
 
         // Returns list of units, what have promt in their username or fullname
@@ -80,8 +81,28 @@ namespace ArchiveSearchEngine.Database
             return units;
         }
 
+        // Returns unit found by his id (throws an exception, if not exists)
+        public NonUser GetUnit(int id)
+        {
+            using (SqliteDataReader reader = new SqliteCommand(
+                $"SELECT * FROM NonUserTable WHERE id = {id}",
+                _connection).ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    return new NonUser(Convert.ToInt32(reader["id"]), (string)reader["fullname"],
+                        (string)reader["post"], (string)reader["struct_division"]);
+                }
+                else
+                {
+                    throw new Exception($"Человек с id: {id} не найден в справочнике");
+                }
+            }
+        }
+
         // Changes [fullname, post, struct_division] of unit with exact id
-        public void UpdateUser(NonUser unit)
+        public void UpdateUnit(NonUser unit)
         {
             unit.Fullname = unit.Fullname.Replace("'", "");
             unit.Post = unit.Post.Replace("'", "");
@@ -96,9 +117,31 @@ namespace ArchiveSearchEngine.Database
         }
 
         // Deletes unit with exact id
-        public void DeleteUser(int id)
+        public void DeleteUnit(int id)
         {
             new SqliteCommand($"DELETE FROM NonUserTable WHERE id = {id}", _connection).ExecuteNonQuery();
+        }
+
+        // Returns true, if unit with exact id exists in database
+        public bool UnitExists(int id)
+        {
+            using (SqliteDataReader reader = new SqliteCommand(
+                $"SELECT id FROM NonUserTable WHERE id = {id}",
+                _connection).ExecuteReader())
+            {
+                return reader.HasRows;
+            }
+        }
+
+        public void ImportFromCSV(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+                NewUnit(values[0], values[1], values[2]);
+            }
         }
     }
 }
